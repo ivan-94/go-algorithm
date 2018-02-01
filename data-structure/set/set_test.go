58895d55450836b5d1b5d194862e40c160fc2038
carney520 <carney520@hotmail.com>
2018-02-01 21:48:57 +0800

    将集合覆盖示例抽离到单独的文件

--- data-structure/set/set_test.go
+++ data-structure/set/set_test.go
@@ -1,7 +1,6 @@
 package set
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -121,65 +120,5 @@ func TestSetSubset(t *testing.T) {
 
 	if !s.Subset(New(DefaultMatch)) {
 		t.Error("集合子集异常, 空集是任何集合的子集")
-	}
-}
-
-// cover 实现集合覆盖
-// members是所有技能集合
-// subsets是members的子集A1， 到An组成的集合。 cover 从subsets中找出覆盖members最高的项
-func cover(members *Set, subsets *Set) (*Set, error) {
-	members = members.Clone()
-	subsets = subsets.Clone()
-	rt := New(DefaultMatch)
-	var maxLen int
-	var maxMember *Set
-	for members.Len() > 0 && subsets.Len() > 0 {
-		maxLen = 0
-
-		// 遍历subset
-		subsets.Each(func(data interface{}, _ int) bool {
-			member, ok := data.(*Set)
-			if !ok {
-				panic("Type error")
-			}
-			i := member.Intersection(members)
-			if i.Len() > maxLen {
-				maxMember = member
-				maxLen = i.Len()
-			}
-			return false
-		})
-
-		// 没有任何子集覆盖
-		if maxLen == 0 {
-			return nil, ErrNotFound
-		}
-		rt.Insert(maxMember)
-		members = members.Diff(maxMember)
-		subsets.Remove(maxMember)
-	}
-	if rt.Len() == 0 {
-		return nil, ErrNotFound
-	}
-	return rt, nil
-}
-
-func Example() {
-	skills := New(DefaultMatch, "c++", "go", "python", "ruby", "java")
-	group := New(func(a, b interface{}) bool {
-		sa := a.(*Set)
-		sb := b.(*Set)
-		return sa.Equal(sb)
-	},
-		New(DefaultMatch, "python", "go", "ruby"),
-		New(DefaultMatch, "c++", "python"),
-		New(DefaultMatch, "java", "ruby"),
-		New(DefaultMatch, "go", "java", "ruby"),
-	)
-	best, err := cover(skills, group)
-	if err != nil {
-		fmt.Println("覆盖失败")
-	} else {
-		fmt.Println(best)
 	}
 }
